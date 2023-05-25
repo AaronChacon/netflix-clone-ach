56bb5185220a5a8cbbab155b5c7c4e46a6e273c9
Aaron Chacon <aaronch.dev@gmail.com>
2023-05-24 22:05:19 -0300

    feat: all banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,28 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Banner.css";
+import requests from "../app/request";
+import axios from "../app/axios";
 
 const Banner = () => {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      let request = await axios.get(requests.fetchNexflixOriginals);
+
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length)
+        ]
+      );
+
+      return request;
+    }
+
+    fetchData();
+  }, []);
+
+  console.log(movie);
+
+  const bannerBg = movie
+    ? `"https://image.tmdb.org/t/p/original/${movie?.backdrop_path}"`
+    : "https://upload.wikimedia.org/wikipedia/commons/c/cd/Black_flag.svg";
+
   return (
     <div
       className="banner"
       style={{
         backgroundSize: "cover",
-        backgroundPosition: "center center",
-        backgroundImage: `url(https://upload.wikimedia.org/wikipedia/commons/c/cd/Black_flag.svg)`,
+        // backgroundPosition: "center center",
+        backgroundImage: `url(${bannerBg})`,
       }}
     >
       <div className="banner__contents">
-        <h1 className="banner__title">Movie Name</h1>
+        <h1 className="banner__title">
+          {movie?.name || movie?.name || movie?.name}
+        </h1>
+
         <div className="banner__buttons">
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
         </div>
 
-        <h2 className="banner__description">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Et eveniet
-          quam sit, ullam ab autem modi voluptatem sequi maxime nam neque nulla
-          maiores sapiente laborum assumenda ut, ex porro facilis?
-        </h2>
+        <h2 className="banner__description">{movie?.overview}</h2>
       </div>
 
       <div className="banner__fadeBottom"></div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,7 +23,7 @@ const Navbar = () => {
       <div className="navbar__contents">
         <img
           className="navbar__logo"
-          src="https://www.pngall.com/wp-content/uploads/4/Netflix-Logo-HD.png"
+          src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
           alt="logo"
         />
 
